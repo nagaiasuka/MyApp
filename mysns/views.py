25e641ab693bb287b2e5655b0d627c6ff4aa39b4
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-04-10 13:40:56 +0900

    モデl

--- mysns/views.py
+++ mysns/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 def index(request):
+    posts = Posts.objects.all()
     context={
         'message':'初めてのDjangoアプリ作成',
-        'members':['asuka','katatumuri','kanabun']
+        'posts':posts,
         }
     return render(request, 'mysns/index.html',context)
